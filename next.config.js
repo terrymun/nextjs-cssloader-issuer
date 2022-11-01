27040c3056a2103535cb100e2b62ca10c7142e74
Terry Mun <hello@terrymun.com>
2022-11-01 13:44:13 +0100

    chore: added next-transpile-modules to transpile an example library (lodash)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')(['lodash']);
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+});
